157839952425f5f9b2dc1136049843adbaa2a9f7
nachodall <ignaciodallacqua@gmail.com>
2024-03-24 17:46:45 -0300

    problem con las referencias

--- Prácticas/Práctica 1/ej1i.java
+++ Prácticas/Práctica 1/ej1i.java
@@ -1,44 +1,49 @@
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class ej1i {
     public static void main(String[] args) {
-        int[] c = {6,12,6};
-        int i = 0;
+        int[] c = {6, 12, 6};
+        int i = c.length;
         int k = 0;
-        Vector<Integer> parcial = new Vector<>();
-        subsetSum(c, i, k, parcial);
-       
-    }
-    public static Vector<Integer> eliminarElemento(Vector<Integer> vector, int elemento) {
-        Vector<Integer> nuevoVector = new Vector<>();
-        
-        for (int i = 0; i < vector.size(); i++) {
-            if (vector.get(i) != elemento) {
-                nuevoVector.add(vector.get(i));
-            }
+        int r = 0;
+        for (int e : c) {
+            r += e;
         }
-        
-        return nuevoVector;
-    }
-
-    public static Vector<Integer> subsetSum(int[] c, int i, int k, Vector<Integer> parcial){
-        if (k<0){
-            return new Vector<Integer>();
+        ArrayList<Integer> solucion = new ArrayList<>();
+        ArrayList<Integer> parcial = new ArrayList<>();
+        System.out.println(subsetSum(c, i, k, r, parcial, solucion));
+        System.out.println("Elementos del ArrayList:");
+        System.out.println(solucion.isEmpty()); //devuelve vacio - problema con la referencia??
+        for (int elemento : solucion) {
+            System.out.println(elemento);
         }
 
-        if (i==0){
-            if (k==0){
-                return parcial;
+    }
+
+    public static boolean subsetSum(int[] c, int i, int k, int r, ArrayList<Integer> parcial, ArrayList<Integer> solucion) {
+        if (k < 0) {
+            return false;
+        }
+        if (k > r) {
+            return false;
+        }
+        if (i == 0) {
+            if (k == 0) {
+                solucion = new ArrayList<>(parcial); // Crear una nueva instancia de ArrayList y copiar elementos
+                return true;
             } else {
-                return new Vector<Integer>();
+                return false;
             }
         }
-
-        parcial.add(c[i]);
-        return subsetSum(c, i-1, k, parcial);
-        eliminarElemento(parcial, c[i]);
-        return subsetSum(c, i-1, k-c[i], parcial);
+        r -= c[i - 1];
+        if (subsetSum(c, i - 1, k, r, parcial, solucion)) {
+            return true;
+        }
+        parcial.add(c[i - 1]);
+        if (subsetSum(c, i - 1, k - c[i - 1], r, parcial, solucion)) {
+            return true;
+        }
+        parcial.remove(Integer.valueOf(c[i - 1]));
+        return false;
     }
-
 }
-
