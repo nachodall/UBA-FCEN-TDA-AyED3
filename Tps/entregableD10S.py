3aeae55da41aa4e077b8a0a0c0cc11cee4ef30cb
nachodall <ignaciodallacqua@gmail.com>
2024-03-31 00:08:24 -0300

    paso a otra cosa

--- Tps/entregableD10S.py
+++ Tps/entregableD10S.py
@@ -9,7 +9,11 @@ class Jugador:
 
 def buscarFormacionIdeal(jugadores):
     def armarDefensores(usados):
-        return [jugador for jugador in jugadores if jugador not in usados]
+        rta: List[Jugador] = []
+        for jugador in jugadores:
+            if jugador not in usados:
+                rta.append(jugador)
+        return rta
 
     def backtracking(delanterosParcial, defensoresParcial, i, usados, max_ataque, max_defensa, delanteros, defensores):
         #caso base 
@@ -22,12 +26,19 @@ def buscarFormacionIdeal(jugadores):
                 max_ataque = sumaParcialAtaque
                 max_defensa = sumaParcialDefensa
                 delanteros.clear()
-                delanteros.extend(delanterosParcial)
+                delanteros.extend(sorted(delanterosParcial, key=lambda j: j.nombre))  # Ordenar los delanteros por nombre
                 defensores.clear()
-                defensores.extend(defensoresParcial)
-                sorted(delanteros, key = lambda j: j.nombre);
-                sorted(defensores, key = lambda j: j.nombre);
+                defensores.extend(sorted(defensoresParcial, key=lambda j: j.nombre))  # Ordenar los defensores por nombre
+            elif (sumaParcialAtaque == max_ataque) and (sumaParcialDefensa == max_defensa):
+                delanterosParcial = sorted(delanterosParcial, key=lambda j: j.nombre)
+                if delanterosParcial > delanteros:
+                    delanteros.clear()
+                    delanteros.extend(delanterosParcial)  # Ordenar los delanteros por nombre
+                    defensores.clear()
+                    defensores.extend(sorted(defensoresParcial, key=lambda j: j.nombre))  # Ordenar los defensores por nombre
             return
+        
+
 
         #caso "recursivo" 
         for j in range(len(jugadores)):
@@ -54,17 +65,23 @@ def buscarFormacionIdeal(jugadores):
 
     return delanteros, defensores
 
-
+"""
 def main():
-    #T = int(input());
+    T = int(input());
 
-    #for k in range(1,T+1):
-     #   jugadores: List[Jugador] = [];
-      #  for _ in range(10):
-       #     nombre, ataque, defensa = input().split();
-        #    jugadorN = Jugador(nombre, int(ataque), int(defensa));
-         #   jugadores.append(jugadorN);
-        #buscarFormacionIdeal(jugadores);
+    for case in range(1,T+1):
+        jugadores: List[Jugador] = [];
+        for _ in range(10):
+            nombre, ataque, defensa = input().split();
+            jugadorN = Jugador(nombre, int(ataque), int(defensa));
+            jugadores.append(jugadorN);       
+        delanteros, defensores = buscarFormacionIdeal(jugadores)
+    
+        print(f"Case {1}:")
+        print(f"({', '.join(jugador.nombre for jugador in delanteros)})")
+        print(f"({', '.join(jugador.nombre for jugador in defensores)})")
+"""
+def main():
     jugadores = [
     Jugador("sameezahur", 20, 21),
     Jugador("sohelh", 18, 9),
@@ -76,12 +93,10 @@ def main():
     Jugador("brokenarrow", 16, 16),
     Jugador("emotionalblind", 16, 12),
     Jugador("tanaeem", 20, 97)
-    ];
-    respuesta = buscarFormacionIdeal(jugadores); 
-    for jugador in respuesta[0]:
-        print (jugador.nombre);
-    for jugador in respuesta[1]:
-        print (jugador.nombre);
+    ]
+    hola = buscarFormacionIdeal(jugadores)
+    for jugador in hola[0]:
+        print(jugador.nombre)
 
 if __name__ == "__main__":
     main();
